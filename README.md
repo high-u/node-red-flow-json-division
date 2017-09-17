a01ab3efe7ed26440acff956d28688bcc87ea4a8
high-u <zen.crazyd@gmail.com>
2017-09-17 11:46:16 +0900

    update README.md

--- README.md
+++ README.md
@@ -1,27 +1,37 @@
 # node-red-flow-json-division
 
+## Released
+
+|Date|Version|Description|
+|:--:|:-----:|:----------|
+|2017-09-17|0.5.2|Supported Windows and add added `check` parameter.|
+
 ## Feature
 
+* Manage Node-RED's JSON with git.
 * Divide JSON in the clipboard and output it in a file.
+* Divide by node.
+* `flow.json` has the clipboard as it is.
 * for git-repository.
+* Supported
+  * Mac
+  * Windows
 
 ## Description
+
+* Output JSON.
 
 ```
 flowjsondiv [Output directory]
 ```
 
+* Check Clipboard.
+
+```
+flowjsondiv check
+```
+
 ## Install (As a command)
-
-* from github
-
-```
-git clone https://github.com/high-u/node-red-flow-json-division.git
-cd node-red-flow-json-division
-npm install -g
-```
-
-* from npm
 
 ```
 npm install -g node-red-flow-json-division
@@ -29,13 +39,23 @@ npm install -g node-red-flow-json-division
 
 ## Usage (As a command)
 
+* Mac
+
 ```
 flowjsondiv ${HOME}/node-red/flow
+
+flowjsondiv check
+```
+
+* Windows
+
+```
+flowjsondiv %HOMEPATH%¥node-red¥flow
+
+flowjsondiv check
 ```
 
 ## Usage (No Install)
-
-* from github
 
 ```
 git clone https://github.com/high-u/node-red-flow-json-division.git
@@ -55,4 +75,5 @@ cd node-red-flow-json-division
 
 * Mac
 * Windows
-* Linux
+* ~~Linux~~
+
--- flowjsondiv.js
+++ flowjsondiv.js
@@ -2,18 +2,15 @@
 
 var fs = require('fs');
 var mkdirp = require('mkdirp');
-var clip = require("node-clip")();
 var ncp = require("copy-paste");
-
 
 // command parameter libraries
 // https://www.npmjs.com/package/commander
 // https://www.npmjs.com/package/optimist
 
 // get parameter
-var param1 = process.argv[2]; // `-c` or `-f` or `output path` or undefined
-var param2 = process.argv[3]; // `input path (-f)` or `output path (-c)` or undefined
-//var param3 = process.argv[4]; // output-path or undefined
+var param1 = process.argv[2]; // `check` or `output path`
+var param2 = process.argv[3]; // `input file path`
 
 //var specialKeys = { func: ".js", x: ".txt", y: ".txt", template: ".txt" };
 var specialKeys = { func: ".js", template: ".txt" };
@@ -62,29 +59,13 @@ var divisioner = function (json, outputname, outputdir) {
         }
     });
 };
-//console.log(param2);
 
 var clipjson = ncp.paste();
 if (param1 === "check") {
     console.log(clipjson);
     return;
 } else if (param2 == undefined) {
-    
-    console.log(clipjson);
-
-    //var outputfilename = param2;
-    //var outputdirpath = param3;
     var outputdirpath = param1;
-    // no check
-
     divisioner(clipjson, "flow.json", outputdirpath);
-    
-    /*
-    //console.log("clipboard");
-    clip.read("general", function (err, value) {
-        //console.log(value);
-        divisioner(value, "flow.json", outputdirpath);
-    });*/
-
 }
 return;
